fcb5f88fc27ca9ee98e38ea81da47994404d3d3b
Narjis Fatima <161502591+NarjisFatima62@users.noreply.github.com>
2024-05-11 21:00:11 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,10 @@
+//Question Number: 10
+//Adding Comments: Choose two of the programs you've written , & ad atleast one comment to each.If you can't have anythng specific to write beacause your programs are to simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does?
+
+//Author: [Narjis Fatima]
+//Date: [Tuesday, March 12,2024]
+
+//task 9: Printing my favourite number = 10;
+let favouriteNumber: number = 10;
+//reveling my favouriteNumber in a message format.
+console.log(`My favouriteNumber is ${favouriteNumber},`);
